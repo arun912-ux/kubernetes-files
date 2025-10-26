890d71d9920413da3eaa0b7412a966a25873d3af
arun912-ux <arunshanagonda777@gmail.com>
2025-10-26 15:00:28 +0000

    Broadcom Bitnami

--- metallb/install.sh
+++ metallb/install.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # helm install metallb
-helm upgrade --install metallb oci://registry-1.docker.io/bitnamicharts/metallb --namespace metallb --create-namespace
+microk8s helm upgrade --install metallb oci://registry-1.docker.io/bitnamicharts/metallb --namespace metallb --create-namespace --values metallb/values.yml
 
-microk8s kubectl apply -f metallb/addressPool.yml
+microk8s kubectl apply -f metallb/addresspool.yml
 microk8s kubectl apply -f metallb/l2Advertise.yml
 
 
